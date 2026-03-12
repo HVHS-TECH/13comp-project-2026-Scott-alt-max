2bac72458492a8de0cdc68872c5188d970a9f24c
Scott-alt-max <22069sb@hvhs.school.nz>
2026-03-12 13:30:51 +1300

    BIG COMMIT finished reg page css

--- registration.js
+++ registration.js
@@ -1,4 +1,17 @@
 
 
 
-document.getElementById("not-logged-in-box").style.display = ("none");
+//document.getElementById("not-logged-in-box").style.display = ("none");
+//document.getElementById("not-a-user-box").style.display = ("none");
+//document.getElementById("already-a-user-box").style.display = ("none");
+//document.getElementById("sign-up-box").style.display = ("none");
+
+function changeToRegBox(regBox) {
+    var allRegBoxes = document.getElementsByClassName("reg-box");
+    for (var i = 0; i < allRegBoxes.length; i++) {
+        allRegBoxes[i].style.display = ("none");
+    }
+    document.getElementById(regBox).style.display = "flex";
+}
+
+changeToRegBox("not-logged-in-box");
