0e92a8f2710afefc8830807506f42c6bd77693ab
Scott-alt-max <22069sb@hvhs.school.nz>
2026-03-19 13:30:18 +1300

    done pretty much nothing but learnt about c

--- registration.js
+++ registration.js
@@ -1,15 +1,54 @@
+
+
+
+
 var userID;
 
+
+
+/**
+ * Sign up button logic, will check if the user is already signed up
+ * If they are, take them to the already a user box,
+ * if they aren't take them to the sign up page
+ */
 function signUp() {
     console.log("sign up");
-    //googleAuth = runGoogleAuth();
-    //userID = googleAuth.user.uid;
+    googleAuth = runGoogleAuth();
+    userID = googleAuth.user.uid;
 
     var allreadySignedUp = checkSignedUp();
     if (allreadySignedUp) {
-
+        changeToRegBox("already-a-user-box");
+    } else {
+        changeToRegBox("sign-up-box");
     }
 }
+function checkSignedUp() {
+    console.log("working");
+
+    const FILEPATH = "userPublicDetails/";
+    readFirebase(FILEPATH).then((userList) => {
+        console.log(userList);
+        
+        for (let user in userList) {
+            if (user == userID) {
+                console.log("User is already signed up");
+                return true;
+            }
+        }
+        return false;
+    }).catch((error) => {
+        console.log("Error with checking if user is signed up");
+        console.log(error);
+        return null;
+    });
+}
+
+function skipToSignUpPage() {}
+function login() {}
+function submit() {}
+function keepOldAccount() {}
+function makeNewAccount() {}
 
 function changeToRegBox(regBox) {
     var allRegBoxes = document.getElementsByClassName("reg-box");
@@ -18,17 +57,3 @@ function changeToRegBox(regBox) {
     }
     document.getElementById(regBox).style.display = "flex";
 }
-
-function checkSignedUp() {
-    console.log("working");
-
-    const FILEPATH = "userPublicDetails/";
-    readFirebase(FILEPATH).then((userList) => {
-        console.log(userList);
-        
-        for (user in userList, () => {
-            
-        });
-    });
-}
-// changeToRegBox("not-logged-in-box");
